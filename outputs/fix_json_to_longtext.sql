9bd9bd932bf7d65fea22f56de791d9390f41e073
Balaji Nagamalla <balaji.nagamalla@mariadb.com>
2026-03-13 12:33:07 +0530

    chore(cleanup): remove unused outputs directory and stale SQL artifacts
    chore(cleanup): remove unused outputs directory and stale SQL artifacts

--- outputs/fix_json_to_longtext.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE appdb.big_orders MODIFY payload LONGTEXT;
-ALTER TABLE json_poc.json_test MODIFY payload LONGTEXT;
--- outputs/fix_users_to_native_password.sql
+++
@@ -1,4 +0,0 @@
-ALTER USER 'analytics'@'10.%' IDENTIFIED WITH mysql_native_password BY '<SET_PASSWORD_HERE>';
-ALTER USER 'app_ro'@'%' IDENTIFIED WITH mysql_native_password BY '<SET_PASSWORD_HERE>';
-ALTER USER 'app_rw'@'%' IDENTIFIED WITH mysql_native_password BY '<SET_PASSWORD_HERE>';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '<SET_PASSWORD_HERE>';
